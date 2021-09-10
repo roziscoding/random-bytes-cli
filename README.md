ee5b2a9133535d00cbfcc6da5646c6f7e2b33e52
Roz <roz@rjmunhoz.me>
2021-09-10 17:18:49 -0300

    feat: initial release

---
+++ README.md
@@ -0,0 +1,19 @@
+random-bytes-cli
+---
+
+Node.js module to generate random bytes using `crypto`'s module `randomBytes` function.
+
+## Installation
+
+```sh
+$ npm install random-bytes-cli
+```
+
+## Usage
+
+```sh
+$ randombytes <size> <format>
+```
+
+- `size`: number of bytes to generate
+- `format`: output format, one of `hex` or `base64`
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import crypto from 'crypto'
+import * as commander from 'commander'
+
+const program = new commander.Command()
+
+program
+  .version(process.env.npm_package_version || 'unknown')
+  .argument('<size>', 'number of bytes to generate', (x) => {
+    const int = parseInt(x, 10)
+
+    if (isNaN(int)) throw new Error('size must be a number')
+
+    return int
+  })
+  .addArgument(
+    new commander.Argument(
+      '<format>',
+      'format in wich the bytes should be printed'
+    ).choices(['hex', 'base64'])
+  )
+  .action((size: number, format: 'hex' | 'base64') => {
+    console.log(crypto.randomBytes(size).toString(format))
+  })
+
+program.parse(process.argv)
